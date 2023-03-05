5dff6d8afe8394f69256d8695c1e3dfae47ac940
simany27 <simany27@gmail.com>
2023-03-05 20:00:49 +0200

    1
    1

---
+++ src/test/java/Start.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Start {
+    WebDriver wd;
+
+    @Test
+    public void start1(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://trello.com/");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+
+        wd.close();
+        wd.quit();
+    }
+}
